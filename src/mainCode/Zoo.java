a191173830a2ee2f11268115cb28d13407858084
Paule AD <akramaite.paule@gmail.com>
2017-04-24 10:19:25 +0100

    zoo class can create enclosures suitable for animal, also has getter and setters

--- src/mainCode/Zoo.java
+++ src/mainCode/Zoo.java
@@ -1,5 +1,41 @@
 package mainCode;
 
-public class Zoo {
+import java.util.ArrayList;
 
+public class Zoo {
+	private ArrayList<Visitor> visitors;
+	private int ticketPrice;
+	private ArrayList<Enclosure> enclosures;
+	
+	public Zoo(){
+		visitors = new ArrayList<Visitor>();
+		ticketPrice = 20;
+		enclosures = new ArrayList<Enclosure>();
+		
+	}
+	
+	public ArrayList<Visitor> getVisitors(){
+		return new ArrayList<Visitor>(visitors);
+	}
+	
+	public ArrayList<Enclosure> getEnclosures(){
+		return new ArrayList<Enclosure>(enclosures);
+	}
+	
+	public int getTicketPrice(){
+		return ticketPrice;
+	}
+	
+	public void setTicketPrice(int newPrice){
+		ticketPrice = newPrice;
+	}
+	
+	public void addEnclosure(Enclosure enclosure){
+		enclosures.add(enclosure);
+	}
+	
+	public void createSuitableEnclosure(Animal animal){
+		Enclosure enclosure = new Enclosure(animal.getPlotSize(), animal.getBiome());
+		enclosures.add(enclosure);
+	}
 }
--- src/test/TestZoo.java
+++ src/test/TestZoo.java
@@ -2,18 +2,35 @@ package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
+import mainCode.*;
 
 public class TestZoo {
-
-//	@BeforeClass
-//	public static void setUpBeforeClass() throws Exception {
-//	}
-//
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
+	
+	private Zoo testZoo;
+	private Animal testAnimal;
+	
+	@Before
+	public void before(){
+		testAnimal = new Lion("Robo", "male", true);
+		testZoo = new Zoo();
+	}
+	
+	@Test 
+	public void testSetUp(){
+		assertEquals(0, testZoo.getEnclosures().size());
+		assertEquals(20, testZoo.getTicketPrice());
+		testZoo.setTicketPrice(30);
+		assertEquals(30, testZoo.getTicketPrice());
+	}
+	
+	@Test
+	public void testEnclosureCreation(){
+		testZoo.createSuitableEnclosure(testAnimal);
+		Enclosure enclosure = testZoo.getEnclosures().get(0);
+		assertEquals(50, enclosure.getPlotSize());
+		assertEquals(BIOME.SAVANAH, enclosure.getBiome());
+	}
 
 }

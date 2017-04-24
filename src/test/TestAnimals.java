9f0641ef0d997b5b7846d4ef59d76b877fa6fdd9
Paule AD <akramaite.paule@gmail.com>
2017-04-24 12:10:28 +0100

    lion can eat rabbit, rabbit can eat carrot tested

--- src/test/TestAnimals.java
+++ src/test/TestAnimals.java
@@ -2,25 +2,48 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import mainCode.Animal;
 import mainCode.Lion;
 import mainCode.Plant;
+import mainCode.Rabbit;
 
 public class TestAnimals {
 
+	private Plant carrot;
+	private Lion leo;
+	private Rabbit rabbit;
+	
+	
+	@Before
+	public void before(){
+		carrot = new Plant("carrot", 5);
+		leo = new Lion("Leo", "male", true);
+		rabbit = new Rabbit("Zizi", "female", true);
+	}
 
 	@Test
-	public void test() {
-		Plant carrot = new Plant("carrot", 5);
-		Lion leo = new Lion("Leo", "male", true);
+	public void testLionCannotEatCarrot() {
+		
 		boolean result = leo.eat(carrot);
 		assertEquals(false, result);
 		assertEquals(50, leo.checkHunger());
 	}
 	
+	@Test 
+	public void testLionCanEatRabbit(){
+		boolean result = leo.eat(rabbit);
+		assertEquals(true, result);
+		assertEquals(120, leo.checkHunger());
+	}
 	
-
+	@Test
+	public void testRabbitCanEatCarrot() {
+		boolean result = rabbit.eat(carrot);
+		assertEquals(true, result);
+		assertEquals(55, rabbit.checkHunger());
+	}
 }

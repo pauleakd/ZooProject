1ccb7ee62d6b9550d1827b46b4f882cd5acb0bc7
Paule AD <akramaite.paule@gmail.com>
2017-04-26 10:04:06 +0100

    added phoenix class

---
+++ src/mainCode/Phoenix.java
@@ -0,0 +1,26 @@
+package mainCode;
+
+public class Phoenix extends Animal {
+	public Phoenix(String name, String gender, boolean maturity){
+		super(name, gender, maturity);
+		biome = BIOME.SAVANAH;
+		plotSize = 10;
+		carnivoreStatus = false;
+		nutritionalValue = 30;
+		strength = 100;
+		price = 100;
+	}
+	
+	@Override
+	public boolean eat(Animal food){
+			System.out.println("Phoenix " + name +" is still hungry");
+			return false;	
+		}
+
+	@Override 
+	public boolean eat(Plant food){
+		super.eat(food);
+		System.out.println("Phoenix " + name + " " + "ate " + food.getName());
+		return true;
+		}
+}
